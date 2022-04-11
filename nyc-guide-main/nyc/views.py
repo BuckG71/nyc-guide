11bfc9b9b858c3fbc93011e87011399ecd04a22e
1mfarhi <manuelafarhi@sbcglobal.net>
2022-04-11 12:15:50 -0500

    I am adding Readme, updated activity and borough and hellovenue and urls and views

--- nyc-guide-main/nyc/views.py
+++ nyc-guide-main/nyc/views.py
@@ -17,10 +17,17 @@ class BoroughView(View):
             context={'borough': borough, 'activities': boroughs[borough].keys()},
         )
 
-
+# make an activityview class, create a get method for self, request, borough and activity.
 class ActivityView(View):
-    pass
+     def get(self, request, borough, activity):
+        return render(
+            request=request,
+            template_name='activity.html',
+            # context={'borough': borough, 'activities': boroughs[borough][activity].keys()},
+            context={'borough': borough, 'activities': boroughs[borough].keys(), 'activity': activity, 'venues':boroughs[borough][activity].keys()})
+    
 
 
 class VenueView(View):
     pass
+    
